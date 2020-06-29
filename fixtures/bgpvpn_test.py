3b37f0f61b757af16bb7199ea9a92d44150c75d5
Aswani Kumar Reddy G <aswanikr@juniper.net>
2020-06-29 10:17:41 +0530

    Added bgpvpn Testsuite
    closes-jira-bug: CEM-9254
    Change-Id: I5af4cea732ba50c7175fcc14ac9e737f7aab911b

---
+++ fixtures/bgpvpn_test.py
@@ -0,0 +1,102 @@
+from tcutils.util import retry,get_random_name
+from vnc_api.vnc_api import Bgpvpn, RouteListType, RouteTargetList
+import fixtures
+
+class BgpvpnFixture(fixtures.Fixture):
+
+    def __init__(self, connections, name, route_target=None, mode=None):
+        self.connections = connections
+        self.inputs = connections.inputs
+        self.logger = self.connections.logger
+        self.vnc_lib_h = self.connections.get_vnc_lib_h()
+        self.api_s_inspect = self.connections.api_server_inspect
+        self.cn_inspect = self.connections.cn_inspect
+        self.project = self.connections.project_name
+        self.bgpvpn_name = get_random_name('bgpvpn')
+        self.bgpvpn_id = None
+        self.bgpvpn_fq_name = ['default-domain', self.bgpvpn_name]
+        self.bgpvpn_obj = None
+        self.mode = mode
+        self.route_target = route_target or []
+
+    def read(self):
+        if self.bgpvpn_id:
+            self.bgpvpn_obj = self.vnc_lib_h.bgpvpn_read(id=self.agg_id)
+            self.bgpvpn_name = self.bgpvpn_obj.name
+    # end read
+
+    def setUp(self):
+        super(BgpvpnFixture, self).setUp()
+        self.create()
+    # end setup
+
+    def create(self):
+        if not self.bgpvpn_id:
+            project = self.vnc_lib_h.project_read(fq_name=['default-domain', self.project_name])
+            bgpvpn = Bgpvpn(name=self.bgpvpn_name, parent_obj=project)
+            bgpvpn.set_route_target_list(RouteTargetList(self.route_target))
+            self.bgpvpn_id = self.vnc_lib_h.bgpvpn_create(bgpvpn)
+            self.logger.info('created Bgpvpn %s'%self.bgpvpn_name)
+            self.read()
+    # end create
+
+    def associate_bgpvpn_to_vn(self, vn_obj):
+        vn_obj.add_bgpvpn(self.bgpvpn_obj)
+        self.vnc_lib_h.virtual_network_update(vn_obj)
+
+    def associate_bgpvpn_to_LR(self, lr_obj):
+        lr_obj.add_bgpvpn(self.bgpvpn_obj)
+        self.vnc_lib.logical_router_update(lr_obj)
+  
+    def update_bgpvpn_mode(self, route_target=[]):
+        self.bgpvpn_obj.set_route_target_list(RouteTargetList(route_target))
+        self.vnc_lib.bgpvpn_update(bgpvpn)
+
+    def disassociate_bgpvpn_from_vn(self,vn_obj):
+        vn_obj.delete_bgpvpn(self.bgpvpn_obj)
+        self.vnc_lib_h.virtual_network_update(vn_obj)
+
+    def disassociate_bgpvpn_from_lr(self,lr_obj):
+        lr_obj.delete_bgpvpn(self.bgpvpn_obj)
+        self.vnc_lib_h.virtual_network_update(lr_obj)
+
+    def update_rt(self, rt=[]):
+        self.bgpvpn_obj.set_route_target_list(RouteTargetList(rt))
+        self.vnc_lib_h.bgpvpn_update(self.bgpvpn_obj)
+ 
+    @retry(delay=1, tries=10)
+    def verify_route_target_in_control(self, vn_fixture, vm_fixture, prefix = '', search_value = ''):
+        search_in_cn = prefix
+        found_value = True
+        for cn in vm_fixture.get_control_nodes():
+            found_value = found_value and re.findall(search_value, str(
+                self.cn_inspect[cn
+                ].get_cn_route_table_entry(search_in_cn,
+                vn_fixture.vn_fq_name+":"+vn_fixture.vn_name)[0]))
+            self.logger.info('Route prefix were found in control node %s'%cn)
+        return True if found_value else False
+    # end verify_route_target_in_control
+
+    @retry(delay=1, tries=10)
+    def verify_prefix_in_control(self, vn_fixture, vm_fixture, prefix = '', search_value = ''):
+        search_in_cn = prefix
+        found_value = True
+        for cn in vm_fixture.get_control_nodes():
+            found_value = found_value and re.findall(search_value, str(
+                self.cn_inspect[cn
+                ].get_cn_route_table_entry(search_in_cn,
+                vn_fixture.vn_fq_name+":"+vn_fixture.vn_name)[0]))
+            self.logger.info('Route prefix were found in control node %s'%cn)
+        return True if found_value else False
+    # end verify_prefix_in_control
+
+    def delete(self):
+        self.logger.info('Deleting Bgpvpn %s'%self.bgpvpn_name)
+        self.vnc_lib_h.bgpvpn_delete(id=self.bgpvpn_id)
+    # end delete
+
+    def cleanUp(self):
+        self.delete()
+        self.logger.info('Deleted Bgpvpn %s' % self.bgpvpn_name)
+        super(BgpvpnFixture, self).cleanUp()
+    # end cleanup
---
+++ serial_scripts/bgpvpn/base.py
@@ -0,0 +1,148 @@
+from common.base import GenericTestBase
+from tcutils.util import get_random_name, get_random_cidr
+
+class BaseBgpvpn(GenericTestBase):
+    
+    @classmethod
+    def setUpClass(cls):
+        super(BaseBgpvpn, cls).setUpClass()
+        cls.quantum_h = cls.connections.quantum_h
+        cls.nova_h = cls.connections.nova_h
+        cls.vnc_lib = cls.connections.vnc_lib
+        cls.agent_inspect = cls.connections.agent_inspect
+        cls.cn_inspect = cls.connections.cn_inspect
+        cls.analytics_obj = cls.connections.analytics_obj
+        cls.create_physical_router()
+        cls.create_bgp_group_on_mx()
+        cls.create_vrf_on_mx()
+    # end setUpClass
+
+    @classmethod
+    def tearDownClass(cls):
+        super(BaseBgpvpn, cls).tearDownClass()
+    # end tearDownClass
+
+    def setUp(self):
+        super(BaseBgpvpn, self).setUp()
+
+    def tearDown(self):
+        super(BaseBgpvpn, self).tearDown()
+
+    def create_basic_config(self, forwarding_mode='l2_l3'):
+        vn_name = get_random_name('bgpvpn_vn')
+        vm_name = get_random_name('bgpvpn_vm')
+        vn_subnets = [get_random_cidr()]
+        self.vn_fixture = self.create_vn(vn_name, vn_subnets,forwarding_mode=forwarding_mode)
+        self.vm_fixture = self.create_vm(vn_fixture, vm_name, image_name='cirros')
+        assert self.vm_fixture.wait_till_vm_is_up()
+
+    def create_lr_config(self, forwarding_mode='l2_l3'):
+        self.create_basic_config('l2_l3')
+        vn2_name = get_random_name('bgpvpn_v2')
+        vm2_name = get_random_name('bgpvpn_vm2')
+        vn2_subnets = [get_random_cidr()]
+        self.vn2_fixture = self.create_vn(vn2_name, vn2_subnets,forwarding_mode=forwarding_mode)
+        self.vm2_fixture = self.create_vm(vn2_fixture, vm2_name, image_name='cirros')
+        assert self.vm2_fixture.wait_till_vm_is_up()
+        self.lr = self.create_lr([self.vn_fixture,self.vn2_fixture])
+        self.lr_obj = self.vnc_api_h.logical_router_read(fq_name=self.lr.lr_fq_name)
+        self.addCleanup(self.lr.delete)
+
+    def create_physical_router(self):
+        device_dict = list(self.inputs.physical_routers_data.values())[0]
+        self.phy_router_obj = PhysicalRouterFixture(
+                device_dict['name'],
+                device_dict['mgmt_ip'],
+                asn=device_dict['asn'],
+                model=device_dict.get('model', 'mx'),
+                vendor=device_dict.get('vendor', 'juniper'),
+                ssh_username=device_dict.get('ssh_username'),
+                ssh_password=device_dict.get('ssh_password'),
+                tunnel_ip=device_dict.get('tunnel_ip'),
+                ports=device_dict.get('ports'),
+                cfgm_ip=self.inputs.cfgm_ip,
+                auth_server_ip=self.inputs.auth_ip,
+                inputs=self.inputs,
+                username=self.inputs.admin_username,
+                password=self.inputs.admin_password,
+                project_name=self.inputs.admin_tenant,
+                domain=self.inputs.admin_domain,
+                orchestrator=self.inputs.orchestrator
+                )
+        self.phy_router_obj.setUp()
+        self.phy_router_obj.verify_bgp_peer()
+
+    def create_bgp_group_on_mx(self):
+        if self.inputs.ext_routers:
+            router_params = list(self.inputs.physical_routers_data.values())[0]
+            if router_params['model'] == 'mx':
+                cmd = []
+                cmd.append('set groups bgpvpn routing-options router-id %s' % router_params['mgmt_ip'])
+                cmd.append('set groups bgpvpn routing-options route-distinguisher-id %s' % router_params['mgmt_ip'])
+                cmd.append('set groups bgpvpn routing-options autonomous-system %s' % router_params['asn'])
+                cmd.append('set groups bgpvpn protocols bgp group bgpvpn type internal')
+                cmd.append('set groups bgpvpn protocols bgp group bgpvpn multihop')
+                cmd.append('set groups bgpvpn protocols bgp group bgpvpn local-address %s' % router_params['mgmt_ip'])
+                cmd.append('set groups bgpvpn protocols bgp group bgpvpn hold-time 90')
+                cmd.append('set groups bgpvpn protocols bgp group bgpvpn keep all')
+                cmd.append('set groups bgpvpn protocols bgp group bgpvpn family inet-vpn unicast')
+                cmd.append('set groups bgpvpn protocols bgp group bgpvpn family inet6-vpn unicast')
+                cmd.append('set groups bgpvpn protocols bgp group bgpvpn family evpn signaling')
+                cmd.append('set groups bgpvpn protocols bgp group bgpvpn family route-target')
+                cmd.append('set groups bgpvpn protocols bgp group bgpvpn local-as %s' % router_params['asn'])
+                for node in self.inputs.bgp_control_ips:
+                    cmd.append('set groups bgpvpn protocols bgp group bgpvpn neighbor %s peer-as %s' % (node, router_params['asn']))
+                cmd.append('set apply-groups bgpvpn')
+                cmds.append('activate protocols bgp group bgpvpn')
+                cli_output = self.run_mx_cmds(router_params['mgmt_ip'],cmd, timeout = 120)
+                cleanup_cmds = []
+                cleanup_cmds.append('delete groups bgpvpn')
+                cleanup_cmds.append('delete apply-groups bgpvpn')
+                self.addCleanup(self.run_mx_cmds, router_params['mgmt_ip'], cleanup_cmds)
+    
+    def create_vrf_on_mx(self):
+        self.logger.info("Create VRF configuration in MX")
+        router_params = list(self.inputs.physical_routers_data.values())[0]
+        #ri_interface = router_params["vrf_interface"]
+        rd = "64512:2223"
+        rt = "target:64512:2223"
+        static_route = ["74.74.74.74/32"]
+        cmds = []
+        cmds.append("set groups bgpvpn routing-instances bgpvpn_vrf instance-type vrf")
+        #cmds.append("set groups bgpvpn routing-instances bgpvpn_vrf interface %s" %ri_interface)
+        cmds.append("set groups bgpvpn routing-instances bgpvpn_vrf route-distinguisher %s" %rd)
+        cmds.append("set groups bgpvpn routing-instances bgpvpn_vrf vrf-target %s" %rt)
+        cmds.append("set groups bgpvpn routing-instances bgpvpn_vrf vrf-table-label")
+        cmds.append("set groups bgpvpn routing-instances bgpvpn_vrf routing-options static route %s discard " %static_route[0])
+        cleanup_cmds = []
+        self.run_mx_cmds(mx_config['control_ip'], cmds, cleanup_cmds)
+
+    def run_mx_cmds(self, device_ip, cli_cmds, ignore_errors=False):
+
+        mx_handle = NetconfConnection(host=device_ip)
+        mx_handle.connect()
+        cli_output = mx_handle.config(
+            stmts=cli_cmds, ignore_errors=ignore_errors, timeout=120)
+        mx_handle.disconnect()
+        assert cli_output[0], "Not able to push config to mx"
+        return cli_output
+    
+    def ping_from_mx(self,mx_ip, cmd):
+        cli_output = self.run_mx_cmds(mx_ip, cmd)
+        return cli_output
+
+    def change_bgp_router_state(self,state=True):
+        bgp_obj = self.phy_router_obj.bgp_router
+        bgp_params = bgp_obj.get_bgp_router_parameters()
+        bgp_params.set_admin_down(state)
+        self.vnc_lib_h.bgp_router_update(bgp_obj)
+        cn_bgp_entry = self.cn_inspect[
+            self.inputs.bgp_control_ips[0]].get_cn_bgp_neigh_entry(encoding='BGP')
+        for entry in cn_bgp_entry:
+            if state:
+                if (entry['encoding'] == 'BGP') and (entry['admin_down'] == 'true'):
+                    return True
+            if not state:
+                if (entry['encoding'] == 'BGP') and (entry['admin_down'] == 'false'):
+                    return True
+        return False
