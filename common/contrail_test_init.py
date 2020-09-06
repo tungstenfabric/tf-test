de7df120f8fc4dfe15286f20e1296e454ff65301
Arina Altynova <aaltynova@progmaticlab.com>
2020-09-06 04:34:51 +0300

    [~]  podman/docker for overcloud [tf-test]
    Change-Id: I08f66dab2de7b48816cb31ba293a4ec1794befd7

--- common/contrail_test_init.py
+++ common/contrail_test_init.py
@@ -739,7 +739,7 @@ class TestInputs(with_metaclass(Singleton, object)):
             self.vro_username = self.vro_server['username']
             self.vro_password = self.vro_server['password']
             self.vro_port = str(self.vro_server['port'])
-                
+
 
     def get_os_env(self, var, default=''):
         if var in os.environ:
@@ -803,7 +803,7 @@ class TestInputs(with_metaclass(Singleton, object)):
         host_dict['containers'] = {}
         if  host_dict.get('type', None) == 'esxi':
             return
-        cmd = 'docker ps -a 2>/dev/null | grep -v "/pause\|/usr/bin/pod\|nova_api_\|contrail.*init\|init.*contrail\|provisioner" | awk \'{print $NF}\''
+        cmd = 'docker ps -a 2>/dev/null | grep -v "/pause\|/usr/bin/pod\|nova_api_\|contrail.*init\|init.*contrail\|provisioner\|placement" | awk \'{print $NF}\''
         output = self.run_cmd_on_server(host_dict['host_ip'], cmd, as_sudo=True)
         # If not a docker cluster, return
         if not output:
@@ -814,7 +814,7 @@ class TestInputs(with_metaclass(Singleton, object)):
         nodemgr_cntrs = [x for x in containers if 'nodemgr' in x]
         containers = set(containers) - set(nodemgr_cntrs)
 
-        # Observed in Openshift scenario, recent container-name changes causing issue picking wrong container which is down/inactive after fail-over 
+        # Observed in Openshift scenario, recent container-name changes causing issue picking wrong container which is down/inactive after fail-over
         # and solving this with Sorting the Set and as this is simple sorting only and so should not impact any other scneario like Openstack/K8s etc
         nodemgr_cntrs = sorted(nodemgr_cntrs, reverse=True)
         containers = sorted(containers, reverse=True)
@@ -1311,7 +1311,7 @@ class ContrailTestInit(object):
 
     def is_container_up(self, host, service):
         container = self.host_data[host]['containers'][service]
-        cmd = "docker ps -f NAME=%s -f status=running 2>/dev/null"%container
+        cmd = "docker ps -f name=%s -f status=running 2>/dev/null"%container
         for i in range(3):
             output = self.run_cmd_on_server(host, cmd, as_sudo=True)
             if not output or 'Up' not in output:
@@ -1455,8 +1455,8 @@ class ContrailTestInit(object):
         for host in bgp_ips:
             host_name = self.host_data[host]['name']
             issue_cmd = "python /usr/share/contrail-utils/provision_control.py \
-			--host_name '%s' --host_ip '%s' --router_asn '%s' \
-			--api_server_ip '%s' --api_server_port '%s' --oper '%s'" % (host_name,
+                --host_name '%s' --host_ip '%s' --router_asn '%s' \
+                --api_server_ip '%s' --api_server_port '%s' --oper '%s'" % (host_name,
                                                                host,
                                                                router_asn,
                                                                api_server_ip,
@@ -1484,9 +1484,9 @@ class ContrailTestInit(object):
         username = self.host_data[self.cfgm_ip]['username']
         password = self.host_data[self.cfgm_ip]['password']
         issue_cmd = "python /usr/share/contrail-utils/provision_mx.py \
-			--api_server_ip '%s' --api_server_port '%s' \
-			--router_name '%s' --router_ip '%s'  \
-			--router_asn '%s' --oper '%s'" % (
+            --api_server_ip '%s' --api_server_port '%s' \
+            --router_name '%s' --router_ip '%s'  \
+            --router_asn '%s' --oper '%s'" % (
             api_server_ip, api_server_port,
             router_name, router_ip, router_asn, oper)
         output = self.run_cmd_on_server(
@@ -1723,7 +1723,7 @@ class ContrailTestInit(object):
     def enable_vro(self, knob=False):
         self.vro_based = knob
     #end enable_vro
-    
+
 def _parse_args( args_str):
     parser = argparse.ArgumentParser()
     args, remaining_argv = parser.parse_known_args(args_str.split())
