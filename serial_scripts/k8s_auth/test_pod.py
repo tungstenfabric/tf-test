a9667eeec7fa8bf460a5c296c0420a0e825521af
nuthanc <nuthanc@juniper.net>
2020-09-21 03:16:53 -0700

    Successful pod creation

--- serial_scripts/k8s_auth/test_pod.py
+++ serial_scripts/k8s_auth/test_pod.py
@@ -3,6 +3,7 @@ from tcutils.kubernetes.auth.example_user import ExampleUser
 from tcutils.kubernetes.auth.util import Util
 import os
 
+
 def pod_with_all_operations_for_admin_project_domain():
     resource = {}
     resource['resources'] = ['Pod']
@@ -14,25 +15,27 @@ def pod_with_all_operations_for_custom_user_project_domain():
     admin.create_all(user_name='john', password='c0ntrail123', role='Member',
                      project_name='new_project', domain_name='new_domain')
     Util.source_stackrc(user_name='john', password='c0ntrail123',
-                   project_name='new_project', domain_name='new_domain', auth_url='http://auth:5000/v3')
+                        project_name='new_project', domain_name='new_domain', auth_url='http://auth:5000/v3')
     resource = {}
     resource['resources'] = ['pod']
     create_policy.create_and_apply_policies(resource=resource)
 
-    #Test required operation
-    #Need to be able to create, delete, get Pod
+    # Test required operation
+    # Need to be able to create, delete, get Pod
     Util.create_resource('pod')
 
-    
-    #Shouldn't be able to create Deployment, Service, Ingress, NAD, Namespace, DaemonSet and NetworkPolicy
-
+    # Shouldn't be able to create Deployment, Service, Ingress, NAD, Namespace, DaemonSet and NetworkPolicy
 
 
 # pod_with_all_operations_for_admin_project_domain()
 # pod_with_all_operations_for_custom_user_project_domain()
 def test():
-    Util.source_stackrc(user_name='john', password='c0ntrail123',
-                   project_name='new_project', domain_name='new_domain', auth_url='http://auth:5000/v3')
+    admin = ExampleUser.admin()
+    # admin.create_all(user_name='john', password='c0ntrail123', role='Member',
+    #                  project_name='new_project', domain_name='new_domain')
+    Util.source_stackrc(user_name='admin', password='password', project_name='admin',
+                        domain_name='admin_domain', auth_url=admin.auth_url)
     Util.create_resource('pod')
+
 
 test()
--- tcutils/kubernetes/auth/util.py
+++ tcutils/kubernetes/auth/util.py
@@ -3,7 +3,8 @@ from subprocess import check_output
 import logging
 logging.basicConfig(
     format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO, datefmt='%d-%b-%y %H:%M:%S')
-#MSG Create a Logger class
+# MSG Create a Logger class
+
 
 class Util:
     cwd = os.path.dirname(os.path.realpath(__file__))
@@ -17,6 +18,12 @@ class Util:
         'ingress': f'{cwd}/templates/ingress.yaml',
         'daemonset': f'{cwd}/templates/daemonset.yaml',
     }
+
+    @staticmethod
+    def exec_kubectl_cmd_on_file(verb, template_file):
+        kubectl = 'kubectl -v=5 --insecure-skip-tls-verify=true -s https://192.168.30.29:6443'
+        return check_output(f'{kubectl} {verb} -f {template_file}', shell=True, universal_newlines=True)
+
     @staticmethod
     def source_stackrc(user_name='admin', password='password', project_name='admin', domain_name='admin_domain', auth_url=None):
         os.environ['OS_IDENTITY_API_VERSION'] = '3'
@@ -27,15 +34,13 @@ class Util:
         os.environ['OS_PASSWORD'] = password
         os.environ['OS_AUTH_URL'] = auth_url
         os.environ['OS_DOMAIN_NAME'] = domain_name
-        
 
     @staticmethod
     def create_resource(resource):
         try:
-            output = check_output(f'kubectl create -f {Util.templates[resource]}', shell=True, universal_newlines=True)
+            output = Util.exec_kubectl_cmd_on_file(
+                verb='create', template_file=Util.templates[resource])
             if 'created' in output:
                 logging.info(f'{resource} created')
         except Exception as e:
             logging.error(e)
-
-        
