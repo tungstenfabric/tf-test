67978b37774e735cab57f871f755e479da5af4a3
nuthanc <nuthanc@juniper.net>
2020-09-24 10:10:44 -0700

    v1 with common method for create and delete

--- tcutils/kubernetes/auth/test_beta.py
+++ tcutils/kubernetes/auth/test_beta.py
@@ -17,23 +17,10 @@ def all_operations_for_admin_project_domain():
 
     Util.source_stackrc(user_name='admin', password='password',
                         project_name='admin', domain_name='admin_domain', auth_url=admin.auth_url)
-    Util.create_resource('pod')
-    Util.create_resource('deployment')
-    Util.create_resource('service')
-    Util.create_resource('namespace')
-    Util.create_resource('network_attachment_definition')
-    Util.create_resource('network_policy')
-    Util.create_resource('ingress')
-    Util.create_resource('daemonset')
-
-    Util.delete_resource('pod')
-    Util.delete_resource('deployment')
-    Util.delete_resource('service')
-    Util.delete_resource('namespace')
-    Util.delete_resource('network_attachment_definition')
-    Util.delete_resource('network_policy')
-    Util.delete_resource('ingress')
-    Util.delete_resource('daemonset')
+    Util.resource(verb='create', resource_list=[
+                  'pod', 'deployment', 'service', 'namespace', 'network_attachment_definition', 'network_policy', 'ingress', 'daemonset'])
+    Util.resource(verb='delete', resource_list=[
+                  'pod', 'deployment', 'service', 'namespace', 'network_attachment_definition', 'network_policy', 'ingress', 'daemonset'])
 
 
 def pod_with_all_operations_for_custom_user_project_domain():
@@ -44,23 +31,10 @@ def pod_with_all_operations_for_custom_user_project_domain():
 
     Util.source_stackrc(user_name='john', password='c0ntrail123',
                         project_name='new_project', domain_name='new_domain', auth_url=admin.auth_url)
-    Util.create_resource('pod')
-    Util.create_resource('deployment')
-    Util.create_resource('service')
-    Util.create_resource('namespace')
-    Util.create_resource('network_attachment_definition')
-    Util.create_resource('network_policy')
-    Util.create_resource('ingress')
-    Util.create_resource('daemonset')
-
-    Util.delete_resource('pod')
-    Util.delete_resource('deployment')
-    Util.delete_resource('service')
-    Util.delete_resource('namespace')
-    Util.delete_resource('network_attachment_definition')
-    Util.delete_resource('network_policy')
-    Util.delete_resource('ingress')
-    Util.delete_resource('daemonset')
+    Util.resource(verb='create', resource_list=[
+                  'pod', 'deployment', 'service', 'namespace', 'network_attachment_definition', 'network_policy', 'ingress', 'daemonset'])
+    Util.resource(verb='delete', resource_list=[
+                  'pod', 'deployment', 'service', 'namespace', 'network_attachment_definition', 'network_policy', 'ingress', 'daemonset'])
 
 
 all_operations_for_admin_project_domain()
--- tcutils/kubernetes/auth/util.py
+++ tcutils/kubernetes/auth/util.py
@@ -23,7 +23,6 @@ class Util:
     @staticmethod
     def exec_kubectl_cmd_on_file(verb, template_file):
         kubectl = 'kubectl -v=5 --insecure-skip-tls-verify=true -s https://192.168.30.29:6443'
-        # MSG USE Popen instead of check_output
         cmd = shlex.split(f'{kubectl} {verb} -f {template_file}')
         p = Popen(cmd, stdout=PIPE, stderr=PIPE, universal_newlines=True)
         output, error = p.communicate()
@@ -41,31 +40,18 @@ class Util:
         os.environ['OS_AUTH_URL'] = auth_url
         os.environ['OS_DOMAIN_NAME'] = domain_name
 
-    @staticmethod
-    def create_resource(resource):
-        output, error = Util.exec_kubectl_cmd_on_file(
-            verb='create', template_file=Util.templates[resource])
-        if 'created' in output:
-            logging.info(f'{resource} created')
-        elif 'forbidden' in error:
-            logging.info(f'{resource} creation forbidden')
-        else:
-            errorObject = error.split("[")[1].split("]")[0]
-            import json
-            errorMessage = json.loads(errorObject)['message']
-            logging.error(errorMessage)
-
 
     @staticmethod
-    def delete_resource(resource):
-        output, error = Util.exec_kubectl_cmd_on_file(
-            verb='delete', template_file=Util.templates[resource])
-        if 'deleted' in output:
-            logging.info(f'{resource} deleted')
-        elif 'forbidden' in error:
-            logging.info(f'{resource} deletion forbidden')
-        else:
-            errorObject = error.split("[")[1].split("]")[0]
-            import json
-            errorMessage = json.loads(errorObject)['message']
-            logging.error(errorMessage)
+    def resource(verb, resource_list):
+        for resource in resource_list:
+            output, error = Util.exec_kubectl_cmd_on_file(
+                verb=verb, template_file=Util.templates[resource])
+            if verb in output:
+                logging.info(f'{verb} {resource} successful')
+            elif 'forbidden' in error:
+                logging.info(f'{verb} {resource} forbidden')
+            else:
+                errorObject = error.split("[")[1].split("]")[0]
+                import json
+                errorMessage = json.loads(errorObject)['message']
+                logging.error(errorMessage)
