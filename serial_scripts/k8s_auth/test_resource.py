8fbbf4fb3b234f74a4570c80e1db0787e4b8b4d2
nuthanc <nuthanc@juniper.net>
2020-10-27 09:48:30 -0700

    Add utility methods and all operation for admin and custom user for deployment

--- serial_scripts/k8s_auth/test_resource.py
+++ serial_scripts/k8s_auth/test_resource.py
@@ -4,10 +4,12 @@ from tcutils.kubernetes.auth.util import Util
 import os
 
 # Tested and working
+
+
 def pod_with_all_operations_for_custom_user_project_domain():
     admin = ExampleUser.admin()
-    admin.create_all(user_name='john', password='c0ntrail123', role='Member',
-                     project_name='new_project', domain_name='new_domain')
+    admin.create_all(user_name='test', password='c0ntrail123', role='Member',
+                     project_name='test_project', domain_name='test_domain')
     resource = {}
     resource['resources'] = ['pods']
     role_dict = {
@@ -16,39 +18,116 @@ def pod_with_all_operations_for_custom_user_project_domain():
     }
     project_dict = {
         'type': 'project',
-        'values': ['new_project']
+        'values': ['test_project']
     }
     user_dict = {
         "type": 'user',
-        "values": ['john']
+        "values": ['test']
     }
     match = [role_dict, project_dict, user_dict]
     create_policy.create_and_apply_policies(resource=resource, match=match)
 
     # Test required operation
-    Util.source_stackrc(user_name='john', password='c0ntrail123',
-                        project_name='new_project', domain_name='new_domain', auth_url=admin.auth_url)
+    Util.source_stackrc(user_name='test', password='c0ntrail123',
+                        project_name='test_project', domain_name='test_domain', auth_url=admin.auth_url)
     Util.resource(verb='create', resource_list=['pod', 'deployment', 'service', 'namespace',
                                                 'network_attachment_definition', 'network_policy', 'ingress', 'daemonset'])
     Util.resource(verb='delete', resource_list=['pod', 'deployment', 'service', 'namespace',
                                                 'network_attachment_definition', 'network_policy', 'ingress', 'daemonset'])
-    #MSG USE resource_with_expectation method after it is found to be working
+    # MSG USE resource_with_expectation method after it is found to be working
+
+
+# Untested
+def resource_with_operations(resource={}, match=[], resource_expectation_list=[], stackrc_dict={}):
+    create_policy.create_and_apply_policies(resource=resource, match=match)
+    Util.source_stackrc(**stackrc_dict)
+    Util.resource_with_expectation(
+        verb='create', resource_expectation_list=resource_expectation_list)
+    Util.resource_with_expectation(
+        verb='delete', resource_expectation_list=resource_expectation_list)
+
+
+# Untested
+def all_operations_for_admin_project_domain():
+    admin = ExampleUser.admin()
+    stackrc_dict = {
+        'user_name': 'admin',
+        'password': 'password',
+        'project_name': 'admin',
+        'domain_name': 'admin_domain',
+        'auth_url': admin.auth_url
+    }
+    resource_expectation_list = ['pod-expected', 'deployment-expected', 'service-expected', 'namespace-expected',
+                                 'network_attachment_definition-expected', 'network_policy-expected', 'ingress-expected', 'daemonset-expected']
+    resource_with_operations(
+        resource_expectation_list=resource_expectation_list, stackrc_dict=stackrc_dict)
 
 
 # Untested
 def pod_with_all_operations_for_admin_project_domain():
     resource = {}
     resource['resources'] = ['pods']
-    create_policy.create_and_apply_policies(resource=resource)
-    Util.source_stackrc()
-    Util.resource(verb='create', resource_list=['pod', 'deployment', 'service', 'namespace',
-                                                'network_attachment_definition', 'network_policy', 'ingress', 'daemonset'])
-    Util.resource(verb='delete', resource_list=['pod', 'deployment', 'service', 'namespace',
-                                                'network_attachment_definition', 'network_policy', 'ingress', 'daemonset'])
-    Util.resource_with_expectation(verb='create', resource_expectation_list=['pod-expected', 'deployment', 'service', 'namespace',
-                                                                     'network_attachment_definition', 'network_policy', 'ingress', 'daemonset'])
-    Util.resource_with_expectation(verb='delete', resource_expectation_list=['pod-expected', 'deployment', 'service', 'namespace',
-                                                                 'network_attachment_definition', 'network_policy', 'ingress', 'daemonset'])
+    admin = ExampleUser.admin()
+    stackrc_dict = {
+        'user_name': 'admin',
+        'password': 'password',
+        'project_name': 'admin',
+        'domain_name': 'admin_domain',
+        'auth_url': admin.auth_url
+    }
+    resource_expectation_list = ['pod-expected', 'deployment-expected', 'service-expected', 'namespace-expected',
+                                 'network_attachment_definition-expected', 'network_policy-expected', 'ingress-expected', 'daemonset-expected']
+    resource_with_operations(resource=resource,
+                             resource_expectation_list=resource_expectation_list, stackrc_dict=stackrc_dict)
+
+
+# Untested
+# MSG DRY up code for custom_user_project
+def deployment_with_all_operations_for_custom_user_project_domain():
+    admin = ExampleUser.admin()
+    admin.create_all(user_name='test', password='c0ntrail123', role='Member',
+                     project_name='test_project', domain_name='test_domain')
+    resource = {}
+    resource['resources'] = ['deployments']
+    role_dict = {
+        'type': 'role',
+        'values': ['Member']
+    }
+    project_dict = {
+        'type': 'project',
+        'values': ['test_project']
+    }
+    user_dict = {
+        "type": 'user',
+        "values": ['test']
+    }
+    match = [role_dict, project_dict, user_dict]
+
+    # Test required operation
+    stackrc_dict = {
+        'user_name': 'test',
+        'password': 'c0ntrail123',
+        'project_name': 'test_project',
+        'domain_name': 'test_domain',
+        'auth_url': admin.auth_url
+    }
+    resource_expectation_list = ['pod', 'deployment-expected', 'service', 'namespace',
+                                 'network_attachment_definition', 'network_policy', 'ingress', 'daemonset']
+    resource_with_operations(resource=resource, match=match,
+                             resource_expectation_list=resource_expectation_list, stackrc_dict=stackrc_dict)
+
+
+# Untested
+def deployment_with_all_operations_for_admin_project_domain():
+    resource = {}
+    resource['resources'] = ['deployments']
+    resource_expectation_list = ['pod', 'deployment-expected', 'service', 'namespace',
+                                 'network_attachment_definition', 'network_policy', 'ingress', 'daemonset']
+    resource_with_operations(
+        resource=resource, resource_expectation_list=resource_expectation_list)
+
+# MSG Add the above 2 methods for other resources
+
 
 pod_with_all_operations_for_custom_user_project_domain()
 # pod_with_all_operations_for_admin_project_domain()
