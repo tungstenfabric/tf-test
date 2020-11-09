b9b16b7426e11cca693dbd543eaa92d4535c74c2
nuthanc <nuthanc@juniper.net>
2020-11-08 22:33:21 -0800

    Add beta test_individual_resource

--- serial_scripts/k8s_auth/beta_test_resource.py
+++ serial_scripts/k8s_auth/beta_test_resource.py
@@ -9,14 +9,26 @@ import os
 
 # Untested
 # MSG Next to test
-def resource_with_all_operations_for_custom_user_project_domain(res):
+def resource_with_all_operations_for_custom_user_project_domain(res, match, stackrc_dict):
     resource = {'resources': [res]}
-    match, stackrc_dict = ResourceUtil.create_test_user_openstack_objects_and_return_match_list_and_stackrc_dict()
-    # Need to create this list dynamically
-    resource_expectation_list = ['pod-expected', 'deployment', 'service', 'namespace',
+    resource_expectation_list = ['pod', 'deployment', 'service', 'namespace',
                                  'network_attachment_definition', 'network_policy', 'ingress', 'daemonset']
+    # MSG Need to add condition properly here as r and res are different
+    for i,r in enumerate(resource_expectation_list):
+        if r in res:
+            resource_expectation_list[i] = r + ':expected'
     ResourceUtil.create_policy_and_perform_operations(
         resource=resource, match=match, stackrc_dict=stackrc_dict, resource_expectation_list=resource_expectation_list)
+
+
+def test_individual_resource_with_all_operations_for_custom_user_project_domain():
+    match, stackrc_dict = ResourceUtil.create_test_user_openstack_objects_and_return_match_list_and_stackrc_dict()
+    resource_list = ['pods', 'deployments', 'services', 'namespaces',
+                     'network-attachment-definitions', 'networkpolicies', 'ingresses', 'daemonsets']
+    for resource in resource_list:
+        resource_with_all_operations_for_custom_user_project_domain(resource, match, stackrc_dict)
+
+test_individual_resource_with_all_operations_for_custom_user_project_domain()
 
 
 # Untested
@@ -31,8 +43,8 @@ def pod_with_all_operations_for_admin_project_domain():
         'domain_name': 'admin_domain',
         'auth_url': admin.auth_url
     }
-    resource_expectation_list = ['pod-expected', 'deployment-expected', 'service-expected', 'namespace-expected',
-                                 'network_attachment_definition-expected', 'network_policy-expected', 'ingress-expected', 'daemonset-expected']
+    resource_expectation_list = ['pod:expected', 'deployment:expected', 'service:expected', 'namespace:expected',
+                                 'network_attachment_definition:expected', 'network_policy:expected', 'ingress:expected', 'daemonset:expected']
     ResourceUtil.create_policy_and_perform_operations(resource=resource,
                                                       resource_expectation_list=resource_expectation_list, stackrc_dict=stackrc_dict)
 
@@ -40,7 +52,7 @@ def pod_with_all_operations_for_admin_project_domain():
 # Untested
 def deployment_with_all_operations_for_custom_user_project_domain():
     resource = {'resources': 'deployments'}
-    resource_expectation_list = ['pod', 'deployment-expected', 'service', 'namespace',
+    resource_expectation_list = ['pod', 'deployment:expected', 'service', 'namespace',
                                  'network_attachment_definit= ion', 'network_policy', 'ingress', 'daemonset']
     match, stackrc_dict = ResourceUtil.create_test_user_openstack_objects_and_return_match_list_and_stackrc_dict()
     ResourceUtil.create_policy_and_perform_operations(resource=resource, match=match,
@@ -59,7 +71,7 @@ def deployment_with_all_operations_for_admin_project_domain():
         'domain_name': 'admin_domain',
         'auth_url': admin.auth_url
     }
-    resource_expectation_list = ['pod', 'deployment-expected', 'service', 'namespace',
+    resource_expectation_list = ['pod', 'deployment:expected', 'service', 'namespace',
                                  'network_attachment_definition', 'network_policy', 'ingress', 'daemonset']
     ResourceUtil.create_policy_and_perform_operations(
         resource=resource, resource_expectation_list=resource_expectation_list, stackrc_dict=stackrc_dict)
