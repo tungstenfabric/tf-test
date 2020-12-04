de22690613b1349ba7c3d553861f9bb0cdb79e58
Gleb Galkin <gleb108@gmail.com>
2020-12-04 14:59:58 +0300

    Fix for SRIOV tests
    partial-jira-bug: CEM-19091
    Change-Id: Icba2b9d94b2ecd95da2a7a217a14493b8ff34a74

--- serial_scripts/sriov/base.py
+++ serial_scripts/sriov/base.py
@@ -1,55 +1,49 @@
 from builtins import range
-import test
+import test_v1
 import time
 import struct
 import socket
 import fixtures
-from common import isolated_creds
 
-class BaseSriovTest(test.BaseTestCase):
+class BaseSriovTest(test_v1.BaseTestCase_v1):
 
     @classmethod
     def setUpClass(cls):
         super(BaseSriovTest, cls).setUpClass()
-        cls.isolated_creds = isolated_creds.IsolatedCreds(cls.__name__, \
-                cls.inputs, input_file = cls.input_file, \
-                logger = cls.logger)
-        cls.isolated_creds.setUp()
-        cls.project = cls.isolated_creds.create_tenant()
-        cls.isolated_creds.create_and_attach_user_to_tenant()
-        cls.inputs = cls.isolated_creds.get_inputs()
-        cls.connections = cls.isolated_creds.get_conections()
+
+        cls.orch = cls.connections.orch
         cls.quantum_h= cls.connections.quantum_h
         cls.nova_h = cls.connections.nova_h
         cls.vnc_lib= cls.connections.vnc_lib
         cls.agent_inspect= cls.connections.agent_inspect
         cls.cn_inspect= cls.connections.cn_inspect
         cls.analytics_obj=cls.connections.analytics_obj
-    #end setUpClass
+        #end setUpClass
 
     @classmethod
     def tearDownClass(cls):
-        cls.isolated_creds.delete_tenant()
         super(BaseSriovTest, cls).tearDownClass()
     #end tearDownClass
 
-    def bringup_interface_forcefully(self, vm_fixture, intf='eth1'):
+    def bringup_interface_forcefully(self, vm_fixture, intf='ens6'):
         cmd = 'ifconfig %s up'%(intf)
         for i in range (5):
           cmd_to_pass = [cmd]
           vm_fixture.run_cmd_on_vm(cmds=cmd_to_pass, as_sudo=True, timeout=60)
           vm_fixture.run_cmd_on_vm(cmds=['ifconfig'], as_sudo=True, timeout=60)
           output = vm_fixture.return_output_cmd_dict['ifconfig']
-          if output and 'eth1' in output:
+          if output and 'ens6' in output:
               break
           else:
               time.sleep(3)
+
     def get_sriov_enabled_compute_list(self):
         sriov_host_name_list=[]
         sriov_host_list=list(self.inputs.sriov_data[0].keys())
         for item in sriov_host_list:
             sriov_host_name_list.append(self.inputs.host_data[item.split('@')[1]]['name'])
         return sriov_host_name_list
+
 
     def get_sriov_physnets(self,compute_name):
         host_key=self.inputs.host_data[compute_name]['username'] + '@' + self.inputs.host_data[compute_name]['host_ip']
