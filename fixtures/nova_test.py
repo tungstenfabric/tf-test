4224010b035cbae76f2e3af99c2989ee62f5c80c
alexey-mr <alexey.morlang@gmail.com>
2020-12-25 00:57:57 +0300

    WIP: experimental
    Change-Id: Ie1e3f69f67a28c0532b3045907549e02c0a65891

--- fixtures/nova_test.py
+++ fixtures/nova_test.py
@@ -570,7 +570,7 @@ class NovaHelper(object):
             nova_services = self.get_nova_compute_service_list()
             for compute_svc in nova_services:
                 if compute_svc.host == node_name or \
-                   self.get_host_name(compute_svc.host) == node_name:
+                   self.get_host_name(compute_svc.host) == self.get_host_name(node_name):
                     node_name = compute_svc.host
                     zone = True
                     break
