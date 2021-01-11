ba95bc36f7868cfd9a1cf84f9bfcf73afc47d946
vappachan <vappachan@juniper.net>
2021-01-11 12:24:21 +0530

    Adding vEPC verification routines closes-jira-bug: CEM-20751 Adding vEPC verification routines.
    Change-Id: I38a3c460bf920f3e3ff0b27f460a9ec43095cbf0

--- serial_scripts/solution/test_orange_solution.py
+++ serial_scripts/solution/test_orange_solution.py
@@ -40,6 +40,20 @@ class OrangeSolutionTest(BaseSolutionsTest):
     @classmethod
     def setup_vepg(cls):
 
+        ## TBD - NEED TO DERIVE VALUES BASED ON INPUTS
+        cls.NB_VSFO_CP_NODES=1
+        cls.NB_VSFO_UP_NODES =1
+        #vSFO CP sizing
+        cls.NB_VSFO_CP_EXT_NIC=1
+        cls.NB_VSFO_CP_SIGIF=3
+        cls.NB_APN_RADIUS=5
+        # vSFO UP sizing
+        cls.NB_VSFO_UP_EXT_NIC=2
+        cls.NB_VSFO_UP_CNNIC=1
+        cls.NB_VSFO_UP_CNIF=2
+        cls.NB_APN=5
+        cls.NB_BGP_PREFIXES_PER_APN=10
+
     #Quota Create.
         cls.quota_env_file=cls.deploy_path+"env/quota.yaml"
         with open(cls.quota_env_file, 'r') as fd:
@@ -157,6 +171,47 @@ class OrangeSolutionTest(BaseSolutionsTest):
                                  template=cls.vepg_template,
                                  timeout_mins=15)
             cls.vepg_stack.setUp()
+            
+        #cls.fip = False
+        stack_name = cls.connections.project_name+'_vepg'
+        op = cls.vepg_stack.heat_client_obj.stacks.get(stack_name).outputs
+        vsfo_fix = dict()
+        for output in op:
+            key = output['output_key']
+
+            for i in range(1,cls.NB_VSFO_CP_NODES+1):
+                vsfo = "vsfo_%s_id" %(i)
+                if key == vsfo:
+                    vsfo_uuid = output['output_value']
+                    vsfo_fix[i] = VMFixture(connections=cls.connections,uuid = vsfo_uuid, image_name = 'VSFO-CP-IMAGE')
+                    vsfo_fix[i].read()
+                    vsfo_fix[i].verify_on_setup()
+                i = i + 1
+
+            for i in range(cls.NB_VSFO_CP_NODES+1 ,cls.NB_VSFO_CP_NODES + cls.NB_VSFO_UP_NODES+1):
+                vsfo = "vsfo_%s_id" %(i)
+                if key == vsfo:
+                    vsfo_uuid = output['output_value']
+                    vsfo_fix[i] = VMFixture(connections=cls.connections,uuid = vsfo_uuid, image_name = 'VSFO-UP-IMAGE')
+                    vsfo_fix[i].read()
+                    vsfo_fix[i].verify_on_setup()
+                i = i+1
+
+            if key == "vrp_31_id":
+                vrp31_uuid = output['output_value']
+                vrp_31 = VMFixture(connections=cls.connections,uuid = vrp31_uuid, image_name = 'VRP-IMAGE')
+                vrp_31.read()
+                vrp_31.verify_on_setup()
+
+            if key == "vrp_32_id":
+                vrp32_uuid = output['output_value']
+                vrp_32 = VMFixture(connections=cls.connections,uuid = vrp32_uuid, image_name = 'VRP-IMAGE')
+                vrp_32.read()
+                vrp_32.verify_on_setup()
+
+        cls.vsfo_fix = vsfo_fix
+        cls.vrp_31 = vrp_31
+        cls.vrp_32 = vrp_32
 
     # Create BGPaaS stacks.
         #Define variables for template file, env file, bgpaas stack and
@@ -269,5 +324,95 @@ class OrangeSolutionTest(BaseSolutionsTest):
         self.logger.info("DONE... Sleeping for 10 secs.")
         return True
     # end test_orange_deploy
+
+    @preposttest_wrapper
+    def test_session_state(self):
+        ''' Test routine to validate BGP/BFD/OSPF session state.
+        '''
+        ret = True
+
+        vsfo_fix = self.vsfo_fix
+        vrp_31 = self.vrp_31
+        vrp_32 = self.vrp_32
+
+        NB_VSFO_CP_NODES= self.NB_VSFO_CP_NODES
+        NB_VSFO_UP_NODES=self.NB_VSFO_UP_NODES
+        NB_VSFO_CP_EXT_NIC=self.NB_VSFO_CP_EXT_NIC
+        NB_VSFO_CP_SIGIF=self.NB_VSFO_CP_SIGIF
+        NB_APN_RADIUS=self.NB_APN_RADIUS
+        NB_VSFO_UP_EXT_NIC=self.NB_VSFO_UP_EXT_NIC
+        NB_VSFO_UP_CNNIC=self.NB_VSFO_UP_CNNIC
+        NB_VSFO_UP_CNIF=self.NB_VSFO_UP_CNIF
+        NB_APN=self.NB_APN
+        NB_BGP_PREFIXES_PER_APN=self.NB_BGP_PREFIXES_PER_APN
+
+        # Validate OSPF session
+
+        cmd = "sshpass -p contrail123 ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -J \"%s@%s\"  -o GlobalKnownHostsFile=/dev/null root@%s  \"cli\" \"show ospf neighbor\" | grep -c Full" %(self.inputs.host_data[vrp_31.vm_node_ip]['username'] ,vrp_31.vm_node_ip,vrp_31.local_ip)
+        op = os.popen(cmd).read()
+        vrp_31_ospf = int(re.match(r'\d',op).group())
+        EXP_SESS=((NB_VSFO_CP_NODES + NB_VSFO_UP_NODES + 1))
+        if EXP_SESS == vrp_31_ospf:
+            self.logger.info("All ospf sessions are UP")
+        else:
+            self.logger.error("All ospf sessions are not UP")
+            ret = False
+
+        # Validate BGP & BFD sessions on vsfo CP nodes
+
+        for i in range(1,NB_VSFO_CP_NODES+1):
+
+            cmd = "sshpass -p contrail123 ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -J \"%s@%s\"  -o GlobalKnownHostsFile=/dev/null root@%s  \"cli\" \"show bgp summary\" | grep -c Establ" %(self.inputs.host_data[vsfo_fix[i].vm_node_ip]['username'] ,vsfo_fix[i].vm_node_ip,vsfo_fix[i].local_ip)
+            op = os.popen(cmd).read()
+            vsfo_cp_session = int(re.match(r'\d+',op).group())
+
+            cmd = "sshpass -p contrail123 ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -J \"%s@%s\"  -o GlobalKnownHostsFile=/dev/null root@%s  \"cli\" \"show bfd session\" | grep ge-0/0 | grep -c Up" %(self.inputs.host_data[vsfo_fix[i].vm_node_ip]['username'] ,vsfo_fix[i].vm_node_ip,vsfo_fix[i].local_ip)
+            op = os.popen(cmd).read()
+            vsfo_cp_bfd_session = int(re.match(r'\d+',op).group())
+
+            EXP_SESS=((2 * (NB_VSFO_CP_SIGIF + NB_APN_RADIUS + 1) * NB_VSFO_CP_EXT_NIC ))
+
+            if EXP_SESS == vsfo_cp_session:
+                self.logger.info("All bgp sessions are UP")
+            else:
+                self.logger.error("All bgp sessions are not UP")
+                ret = False
+
+            if EXP_SESS/2 == vsfo_cp_bfd_session:
+                self.logger.info("All bfd sessions are UP")
+            else:
+                self.logger.error("All bfd sessions are not UP")
+                ret = False
+            i = i + 1
+
+        # Validate BGP & BFD sessions on vsfo CP nodes
+
+        for i in range(NB_VSFO_CP_NODES+1 ,NB_VSFO_CP_NODES + NB_VSFO_UP_NODES+1):
+
+            cmd = "sshpass -p contrail123 ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -J \"%s@%s\"  -o GlobalKnownHostsFile=/dev/null root@%s  \"cli\" \"show bgp summary\" | grep -c Establ" %(self.inputs.host_data[vsfo_fix[i].vm_node_ip]['username'] ,vsfo_fix[i].vm_node_ip,vsfo_fix[i].local_ip)
+            op = os.popen(cmd).read()
+            vsfo_up_session = int(re.match(r'\d+',op).group())
+
+            cmd = "sshpass -p contrail123 ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -J \"%s@%s\"  -o GlobalKnownHostsFile=/dev/null root@%s  \"cli\" \"show bfd session\" | grep ge-0/0 | grep -c Up" %(self.inputs.host_data[vsfo_fix[i].vm_node_ip]['username'] ,vsfo_fix[i].vm_node_ip,vsfo_fix[i].local_ip)
+            op = os.popen(cmd).read()
+            vsfo_up_bfd_session = int(re.match(r'\d+',op).group())
+            
+            EXP_SESS=(( 2*((NB_VSFO_UP_CNNIC * NB_VSFO_UP_CNIF) + (NB_VSFO_UP_EXT_NIC - NB_VSFO_UP_CNNIC) * NB_APN) ))
+
+            if EXP_SESS == vsfo_up_session:
+                self.logger.info("All bgp sessions are UP")
+            else:
+                self.logger.error("All bgp sessions are not UP")
+                ret = False
+
+            if EXP_SESS/2 == vsfo_up_bfd_session:
+                self.logger.info("All bfd sessions are UP")
+            else:
+                self.logger.error("All bfd sessions are not UP")
+                ret = False
+            i = i+1
+
+        return ret
+
 
 #end class OrangeSolutionTest
