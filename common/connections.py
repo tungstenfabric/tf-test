tungstenfabric/tf-test

commit 11179b7d359a14cd792ef93cb7dc2710e8bf2ce7

Adds support for k8s sanity in juju+k8s cluster
partial-jira-bug: CEM-17146
Change-Id: I2a37c2fc75e48f2bc780cc372f76a26e12b7ede6

common/connections.py
@@ -285,22 +285,20 @@ class ContrailConnections(object):
         return self.k8s_cluster
 
     def get_k8s_api_client_handle(self):
-        if self.inputs.orchestrator != 'kubernetes' and self.inputs.slave_orchestrator != 'kubernetes':
+        if self.inputs.orchestrator != 'kubernetes' and \
+            self.inputs.slave_orchestrator != 'kubernetes' and \
+            self.inputs.additional_orchestrator != 'kubernetes':
             return None
         if not getattr(self, 'k8s_client', None):
             if self.inputs.deployer == 'openshift':
                 self.k8s_client = Openshift_client(self.inputs.kube_config_file,
                                                 self.logger)
-            else:
-                if self.inputs.slave_orchestrator == 'kubernetes':
-                    if self.k8s_cluster:
+            elif self.inputs.slave_orchestrator == 'kubernetes' and self.k8s_cluster:
                         self.k8s_client = Kubernetes_client(
                                                 cluster=self.k8s_cluster,
                                                 logger=self.logger)
-                    else:
-                        self.k8s_client = None
-                else:
-                    self.k8s_client = Kubernetes_client(
+            else:
+                self.k8s_client = Kubernetes_client(
                                                 self.inputs.kube_config_file,
                                                 self.logger)
         return self.k8s_client

common/contrail_test_init.py
@@ -538,7 +538,10 @@ class TestInputs(with_metaclass(Singleton, object)):
         test_configs = self.config.get('test_configuration') or {}
         self.orchestrator = deployment_configs.get('orchestrator') or 'openstack'
         self.slave_orchestrator = deployment_configs.get('slave_orchestrator',None)
+        self.additional_orchestrator = deployment_configs.get('additional_orchestrator', None)
         if self.deployer == 'openshift':
+            kube_config_file = OPENSHIFT_CONFIG_FILE
+        if self.deployer == 'juju' or self.additional_orchestrator == 'kubernetes':
             kube_config_file = OPENSHIFT_CONFIG_FILE
         else:
             kube_config_file = K8S_CONFIG_FILE
@@ -595,6 +598,8 @@ class TestInputs(with_metaclass(Singleton, object)):
             self.admin_tenant = self.k8s_cluster_name + '-default'
         elif self.slave_orchestrator == 'kubernetes':
             self.k8s_clusters = test_configs['k8s_nested']['clusters']
+        elif self.additional_orchestrator == 'kubernetes':
+            self.k8s_cluster_name = contrail_configs.get('KUBERNETES_CLUSTER_NAME') or "k8s"
 
         # test specific configs
         self.auth_url = test_configs.get('auth_url') or os.getenv('OS_AUTH_URL',
@@ -1048,7 +1053,7 @@ class ContrailTestInit(object):
         # address_family = read_config_option(self.config,
         #                      'Basic', 'AddressFamily', 'dual')
         self.address_family = 'v4'
-        if self.orchestrator == 'kubernetes':
+        if self.orchestrator == 'kubernetes' or self.additional_orchestrator == 'kubernetes':
             if not os.path.exists(self.kube_config_file):
                  self.copy_file_from_server(self.k8s_master_ip,
                         self.kube_config_file, self.kube_config_file)

common/k8s/base.py
@@ -50,6 +50,13 @@ class BaseK8sTest(GenericTestBase, vnc_api_test.VncLibFixture):
                                                   logger=cls.logger,
                                                   fip_pool_name=cls.inputs.fip_pool_name,
                                                   api_option='contrail')
+        if cls.inputs.additional_orchestrator == 'kubernetes':
+            if 'KUBERNETES_PUBLIC_FIP_POOL' in cls.inputs.contrail_configs:
+                cls.connections.project_name = cls.inputs.contrail_configs['KUBERNETES_PUBLIC_FIP_POOL']['project']
+                cls.connections.domain_name = cls.inputs.contrail_configs['KUBERNETES_PUBLIC_FIP_POOL']['domain']
+            else:
+                cls.connections.project_name = cls.inputs.k8s_cluster_name + '-default'
+                cls.connections.domain_name = cls.inputs.stack_domain
         cls.cluster_connections = []
         if cls.inputs.slave_orchestrator == 'kubernetes':
             for cluster in cls.inputs.k8s_clusters:

fixtures/floating_ip.py
@@ -23,8 +23,6 @@ class FloatingIPFixture(fixtures.Fixture):
                  option=None, uuid=None):
         self.connections = connections
         self.inputs = inputs or connections.inputs
-        if not project_name:
-            project_name = self.inputs.project_name
         self.api_s_inspect = self.connections.api_server_inspect
         self.orch = self.connections.orch
         self.quantum_h = self.connections.quantum_h
@@ -33,7 +31,7 @@ class FloatingIPFixture(fixtures.Fixture):
         self.vnc_lib_h = self.connections.vnc_lib
         self.analytics_obj = self.connections.analytics_obj
 
-        self.project_name = project_name
+        self.project_name = project_name or self.connections.project_name
         self.domain_name = self.connections.domain_name
         self.vn_id = vn_id
         self.vn_name = vn_name

fixtures/k8s/hbs.py
@@ -50,7 +50,7 @@ class HbsFixture(fixtures.Fixture):
         self.leftnad  = None
         self.rightnad  = None
         self.connections = connections
-        self.vnc_lib = connections.get_vnc_lib_h()
+        self.vnc_lib = connections.vnc_lib
         self.agent_inspect = connections.agent_inspect
         self.username = "root"
         self.password = "c0ntrail123"

fixtures/k8s/namespace.py
@@ -102,7 +102,7 @@ class NamespaceFixture(fixtures.Fixture):
     @retry(delay=2, tries=10)
     def verify_namespace_in_contrail_api(self):
         self.project_name = self.get_project_name_for_namespace()
-        self.project_fq_name = '%s:%s' %(self.inputs.admin_domain,
+        self.project_fq_name = '%s:%s' %(self.connections.domain_name,
                                          self.project_name)
         try:
             self.api_s_obj = self.vnc_api_h.project_read(

fixtures/k8s/network_attachment.py
@@ -28,7 +28,7 @@ class NetworkAttachmentFixture(fixtures.Fixture):
         self.spec = {} if spec is None else spec
         self.already_exists = None
         self.connections = connections
-        self.vnc_lib = connections.get_vnc_lib_h()
+        self.vnc_lib = connections.vnc_lib
         self.agent_inspect = connections.agent_inspect
     # end __init__
 

fixtures/k8s/pod.py
@@ -34,7 +34,7 @@ class PodFixture(fixtures.Fixture):
         self.custom_isolation = custom_isolation
         self.fq_network_name = fq_network_name
         self.connections = connections
-        self.vnc_lib = connections.get_vnc_lib_h()
+        self.vnc_lib = connections.vnc_lib
         self.agent_inspect = connections.agent_inspect
 
         self.api_vm_obj = None

commit e0d1f46c5aeebe4a38e4c2e5b63b9c5b1948cd2d

Fixes framework for k8s-nested setup
Fixes regression caused by https://github.com/tungstenfabric/tf-test/commit/11179b7d359a14cd792ef93cb7dc2710e8bf2ce7

closes-jira-bug: CEM-21229
Change-Id: Id81689cf39255b7e4f9010576d35846d57685045

common/connections.py
@@ -293,10 +293,13 @@ class ContrailConnections(object):
             if self.inputs.deployer == 'openshift':
                 self.k8s_client = Openshift_client(self.inputs.kube_config_file,
                                                 self.logger)
-            elif self.inputs.slave_orchestrator == 'kubernetes' and self.k8s_cluster:
-                        self.k8s_client = Kubernetes_client(
-                                                cluster=self.k8s_cluster,
-                                                logger=self.logger)
+            elif self.inputs.slave_orchestrator == 'kubernetes':
+                if self.k8s_cluster:
+                    self.k8s_client = Kubernetes_client(
+                                            cluster=self.k8s_cluster,
+                                            logger=self.logger)
+                else:
+                    self.k8s_client = None
             else:
                 self.k8s_client = Kubernetes_client(
                                                 self.inputs.kube_config_file,