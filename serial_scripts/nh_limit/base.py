e7a8df57a5058b30bea718da0a757f82ce9c6e72
rsetru1 <rsetru@juniper.net>
2021-04-23 19:25:15 +0530

    Adding retries for verifying nh indexes and while configuring vn with route target and when attaching policy "closes-jira-bug: CEM-21603"
    Change-Id: I676832284e7ff90427e0d2d951b19bb92f2fb95b
    Signed-off-by: Revanth Setru <rsetru@juniper.net>

--- serial_scripts/nh_limit/base.py
+++ serial_scripts/nh_limit/base.py
@@ -19,18 +19,35 @@ class TestNHLimit(GenericTestBase):
     vn_fixtures = []
     vm_fixtures = []
 
-    @retry(delay=10, tries=40)
+    @retry(delay=10, tries=60)
     def verify_nh_indexes(self, compute, Range):
         '''
         Description: Number of nexthop indexes are verified on vrouter agent side
         '''
+        result = True
+        nh_indexes_num_cmd_result = False
         cmd = "contrail-tools nh --list | grep Id | wc -l"
-        nh_indexes = run_cmd_on_server(
-            cmd, compute, username='root', password='c0ntrail123')
+        for i in range(0,30):
+            try:
+                self.logger.debug("number of nh indexes verification command run Retry %s" % (i))
+                nh_indexes = run_cmd_on_server(
+                    cmd, compute, username='root', password='c0ntrail123')
+            except Exception as e:
+                time.sleep(2)
+                self.logger.exception("Got exception as %s" % (e))
+                continue
+            nh_indexes_num_cmd_result = True
+            break     
+        if nh_indexes_num_cmd_result == True:
+            self.logger.debug("number of nh indexes verification command ran, Didn't timeout") 
+        else:
+            assert False, "number of nh indexes verification command timed out"
         if Range[0] <= int(nh_indexes) <= Range[1]:
             self.logger.debug("nh_indexes are populated and in range")
         else:
-            assert False, "nh_indexes are not in range"
+            self.logger.debug("nh_indexes are not in range")
+            result = False
+        return result
 
     def set_nh_limit(self, nh_limit, compute, agent_mode=None, mpls_limit=None, modify=False):
         '''
@@ -71,20 +88,22 @@ class TestNHLimit(GenericTestBase):
 
         if agent_mode == 'dpdk':
             cmds = ['docker cp entrypoint.sh vrouter_vrouter-agent-dpdk_1:/entrypoint.sh',
-                    'docker stop vrouter_vrouter-agent-dpdk_1', 'docker start vrouter_vrouter-agent-dpdk_1']
+                    'docker stop vrouter_vrouter-agent_1', 'docker stop vrouter_vrouter-agent-dpdk_1',
+                    'docker start vrouter_vrouter-agent-dpdk_1', 'docker start vrouter_vrouter-agent_1']
         else:
             cmds = ['docker stop vrouter_vrouter-agent_1', 'ifdown vhost0',
                     'cd /etc/contrail/vrouter/; docker-compose down; docker-compose up -d']
         for cmd in cmds:
             run_cmd_on_server(cmd, compute, username='root',
                               password='c0ntrail123')
-        self.verify_nh_limit(compute, nh_limit, mpls_limit)
+        assert self.verify_nh_limit(compute, nh_limit, mpls_limit)
 
-    @retry(delay=3, tries=7)
+    @retry(delay=3, tries=20)
     def verify_nh_limit(self, compute, nh_limit, mpls_limit=None):
         '''
         Description: Nh limit set on vrouter agent side is checked and verified comparing with nhlimit value passed as argument. This is called whenever new nh_limit is set.
         '''
+        result = True
         verify_nh_limit_cmd = "contrail-tools vrouter --info | awk '{print $3}' | awk 'NR==6'"
         nh_limit_set = run_cmd_on_server(
             verify_nh_limit_cmd, compute, username='root', password='c0ntrail123')
@@ -92,8 +111,8 @@ class TestNHLimit(GenericTestBase):
             self.logger.debug(
                 'Desired nh_limit %s is set on agent side' % nh_limit_set)
         else:
-            assert False, "Proper nh_limit is not set"
-            self.logger.debug('nhlimit set is %s' % nh_limit_set)
+            self.logger.debug('proper nhlimit not set %s' % nh_limit_set)
+            result = False
         if mpls_limit is not None:
             verify_mpls_limit_cmd = "contrail-tools vrouter --info | awk '{print $4}' | awk 'NR==7'"
             mpls_limit_set = run_cmd_on_server(
@@ -102,8 +121,9 @@ class TestNHLimit(GenericTestBase):
                 self.logger.debug(
                     'Desired mpls_limit %s is set on agent side' % mpls_limit_set)
             else:
-                assert False, "Proper mpls_limit is not set"
-                self.logger.debug('mplslimit set is %s' % mpls_limit_set)
+                self.logger.debug('proper mplslimit not set %s' % mpls_limit_set)
+                result = False
+        return result
 
     def reset_nh_limit(self, compute, agent_mode=None):
         '''
@@ -164,28 +184,37 @@ class TestNHLimit(GenericTestBase):
     def create_vmvn_for_nhlimittest(self, compute, vn_count):
         for i in range(vn_count):
             vn_fixture = self.create_vn()
-            for i in range(0,5):
-                while True:
-                    try:    
-                        assert vn_fixture.verify_on_setup()
-                    except AttributeError:
-                        self.logger.debug("agent introspect might be down only at this moment")
-                        continue
-                    break
+            for i in range(0,20):
+                try:    
+                    self.logger.debug("vn verification Retry %s" % (i))
+                    assert vn_fixture.verify_on_setup()
+                except AttributeError:
+                    self.logger.debug("agent introspect might be down only at this moment")
+                    time.sleep(2)
+                    continue
+                break
             self.vn_fixtures.append(vn_fixture)
             vm_fixture = self.create_vm(
                 vn_fixture=vn_fixture, node_name=compute)
             assert vm_fixture.wait_till_vm_is_up()
             assert vm_fixture.verify_on_setup()
             self.vm_fixtures.append(vm_fixture)
+        result_add_route_target = False 
         for i in range(len(self.vn_fixtures)):
-            for j in range(0,10):
-                while True:
-                    try:
-                        self.vn_fixtures[i].add_route_target(router_asn=64512, route_target_number=190)
-                    except vnc_api.exceptions.NoIdError:
-                        continue
-                    break
+            for j in range(0,60):
+                try:
+                    self.logger.debug("Route target add Retry %s" % (j))
+                    self.vn_fixtures[i].add_route_target(router_asn=64512, route_target_number=190)
+                except Exception as e:
+                    time.sleep(3)
+                    self.logger.exception("Got exception as %s" % (e))
+                    continue
+                result_add_route_target = True
+                break
+            if result_add_route_target == True:
+                self.logger.debug("Route target added")
+            else:
+                assert False, "Route target add failed"
         vn1_name = self.vn_fixtures[0].vn_fq_name
         vn2_name = self.vn_fixtures[1].vn_fq_name
         rule1 = self._get_network_policy_rule(
@@ -193,7 +222,21 @@ class TestNHLimit(GenericTestBase):
         rule2 = self._get_network_policy_rule(
             src_vn=vn2_name, dst_vn=vn1_name, action='pass')
         vn12_pol = self.create_policy(rules=[rule1, rule2])
-        self.apply_policy(vn12_pol, [self.vn_fixtures[0], self.vn_fixtures[1]])
+        result_add_policy = False
+        for i in range(0,60):
+            try:
+                self.logger.debug("policy apply Retry %s" % (i))
+                self.apply_policy(vn12_pol, [self.vn_fixtures[0], self.vn_fixtures[1]])
+            except Exception as e:
+                time.sleep(3)
+                self.logger.exception("Got exception as %s" % (e))
+                continue
+            result_add_policy = True
+            break
+        if result_add_policy == True:
+            self.logger.debug("Policy applied")
+        else:
+            assert False, "Policy apply failed"
 
     def get_compute(self, agent_mode=None):
         dpdk_computes = []
