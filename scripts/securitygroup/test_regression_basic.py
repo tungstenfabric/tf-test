66aecb096e424f44be62448d618f6a0429f593d6
hkgopal <gkagawad@juniper.net>
2021-11-16 11:23:06 +0530

    Adding a test case to validate the fix for hold flow entry
    closes-jira-bug: CEM-22323
    Change-Id: I939b5f80d50a2e1204586f1deaa8b62c30b13ec4
    
    Signed-off-by: GopalKagawad <gkagawad@juniper.net>
    Change-Id: I57d10251730fb7c3c039ee6098f9febb0c3c6cb3

--- scripts/securitygroup/test_regression_basic.py
+++ scripts/securitygroup/test_regression_basic.py
@@ -183,3 +183,114 @@ class SecurityGroupBasicRegressionTests1(BaseSGTest, VerifySecGroup, ConfigPolic
         #Ping should pass now
         assert vm1_fixture.ping_with_certainty(ip=vm2_fixture.vm_ip,
             expectation=True)
+
+    @preposttest_wrapper
+    def test_sec_group_hold_flow_negative_test(self):
+        """
+        Description: Test hold flow when SG having ingress deny rule
+                     (Added this to validate fix provided for CEM-22323)
+            1. Security group create and delete
+            2. Create security group with custom rules to allow all traffic
+            3. Launch VM with custom created security group and verify
+            4. Test with ping, which should pass
+            5. Update the rules to allow only tcp at ingress
+            6. Test with ping which should fail
+            7. Verify the flow on the compute nodes, there should not be any hold flows created
+        """
+        secgrp_name = get_random_name('test_sec_group')
+        vn_net = get_random_cidrs(self.inputs.get_af())
+        (prefix, prefix_len) = vn_net[0].split('/')
+        rule = [{'protocol': 'icmp',
+                 'dst_addresses': [{'subnet': {'ip_prefix': prefix,
+                    'ip_prefix_len': prefix_len}}],
+                 'dst_ports': [{'start_port': 0, 'end_port': -1}],
+                 'src_ports': [{'start_port': 0, 'end_port': -1}],
+                 'src_addresses': [{'security_group': 'local'}],
+                 },
+                {'protocol': 'icmp',
+                 'src_addresses': [{'subnet': {'ip_prefix': prefix,
+                    'ip_prefix_len': prefix_len}}],
+                 'dst_ports': [{'start_port': 0, 'end_port': -1}],
+                 'src_ports': [{'start_port': 0, 'end_port': -1}],
+                 'dst_addresses': [{'security_group': 'local'}],
+                 }]
+
+        # Create SG
+        sg_fixture = self.config_sec_group(name=secgrp_name, entries=rule)
+        secgrp_id = sg_fixture.secgrp_id
+
+        # Create VN and VMs
+        vn_fixture = self.useFixture(VNFixture(
+            project_name=self.inputs.project_name, connections=self.connections,
+            inputs=self.inputs, subnets=vn_net))
+        assert vn_fixture.verify_on_setup()
+        img_name = self.inputs.get_ci_image() or 'ubuntu-traffic'
+
+        # cannot use set_security_group in vro ,so remove default sg before adding new sg
+        if self.inputs.vro_based:
+            vm1_fixture = self.useFixture(VMFixture(
+                project_name=self.inputs.project_name, connections=self.connections,
+                vn_obj=vn_fixture.obj, image_name=img_name, flavor='contrail_flavor_small'))
+            vm2_fixture = self.useFixture(VMFixture(
+                project_name=self.inputs.project_name, connections=self.connections,
+                vn_obj=vn_fixture.obj, image_name=img_name, flavor='contrail_flavor_small'))
+            assert vm1_fixture.verify_on_setup()
+            assert vm1_fixture.wait_till_vm_is_up()
+            assert vm2_fixture.verify_on_setup()
+            assert vm2_fixture.wait_till_vm_is_up()
+            vm1_fixture.remove_security_group(secgrp='default')
+            vm2_fixture.remove_security_group(secgrp='default')
+            vm1_fixture.add_security_group(secgrp_id)
+            vm2_fixture.add_security_group(secgrp_id)
+        else:
+            vm1_fixture = self.useFixture(VMFixture(
+                project_name=self.inputs.project_name, connections=self.connections,
+                vn_obj=vn_fixture.obj, image_name=img_name, flavor='contrail_flavor_small',sg_ids=[secgrp_id]))
+            vm2_fixture = self.useFixture(VMFixture(
+                project_name=self.inputs.project_name, connections=self.connections,
+                vn_obj=vn_fixture.obj, image_name=img_name, flavor='contrail_flavor_small',sg_ids=[secgrp_id]))
+            assert vm1_fixture.verify_on_setup()
+            assert vm1_fixture.wait_till_vm_is_up()
+            assert vm2_fixture.verify_on_setup()
+            assert vm2_fixture.wait_till_vm_is_up()
+
+        result, msg = vm1_fixture.verify_security_group(secgrp_name)
+        assert result, msg
+        # Ping test, should pass
+        assert vm1_fixture.ping_with_certainty(ip=vm2_fixture.vm_ip,
+            expectation=True)
+        self.logger.info("Ping PASSED as expected")
+
+        # delete all the flows from both the compute nodes
+        vm1_compute_fix = self.compute_fixtures_dict[vm1_fixture.vm_node_ip]
+        vm2_compute_fix = self.compute_fixtures_dict[vm2_fixture.vm_node_ip]
+        vm1_compute_fix.delete_all_flows()
+        vm2_compute_fix.delete_all_flows()
+
+        # updating ingress rule to allow only tcp, in order to check ping is blocked
+        rule[1]['protocol'] = 'tcp'
+        sg_fixture.replace_rules(rule)
+        self.logger.info("Ingress rule to allow only tcp traffic")
+        assert vm1_fixture.ping_with_certainty(ip=vm2_fixture.vm_ip,
+            expectation=False)
+        self.logger.info("Ping FAILED as expected")
+        cmd1 = 'contrail-tools flow --match {}'.format(vm1_fixture.vm_ip)
+        cmd2 = 'contrail-tools flow --match {}'.format(vm2_fixture.vm_ip)
+        matching_flow_src_node = vm1_compute_fix.execute_cmd(cmd1, container=None)
+        msg1 = "was expecting Forward but received different action:"
+        msg2 = "Refer the flow: {}".format(matching_flow_src_node)
+        assert 'Action:F' in matching_flow_src_node, msg1+msg2
+        self.logger.info("Flow with action as FORWARD in source compute")
+        assert 'Action:H' not in matching_flow_src_node, "Hold flow was not expected but " \
+                                                         "got created"
+        self.logger.info("No HOLD Flow entry on source compute")
+        matching_flow_dst_node = vm2_compute_fix.execute_cmd(cmd2, container=None)
+        msg3 = "was expecting Action as Drop but received different action:"
+        msg4 = "Refer the flow: {}".format(matching_flow_dst_node)
+        cond1 = 'Action:D(SG)' in matching_flow_dst_node
+        cond2 = 'Action:D(Unknown)' in matching_flow_dst_node
+        assert (cond1 and cond2), msg3+msg4
+        self.logger.info("Flow with action as DROP in Dest compute")
+        assert 'Action:H' not in matching_flow_dst_node, "Hold flow was not expected but " \
+                                                         "got created"
+        self.logger.info("No HOLD Flow entry on Dest compute")
