e6224843d29433dd6c2fdb96575c710d18886530
Manas Dash <manasd@juniper.net>
2022-03-07 16:57:29 +0530

    closes-jira-bug: CEM-25575 (Solution Test Automation: Support Dynamic Learning with IPVLAN)
    Change-Id: I789b2726624a82a5624614daafcdd7af844cbb6b
    Signed-off-by: Manas Dash <manasd@juniper.net>

--- common/maciplearning/base.py
+++ common/maciplearning/base.py
@@ -1,13 +1,6 @@
-from builtins import range
 import re
-import time
 from common.base import GenericTestBase
 from common.connections import ContrailConnections
-from common import isolated_creds
-from vm_test import VMFixture
-from vn_test import VNFixture
-from tcutils.util import retry
-from string import Template
 from tcutils.commands import ssh, execute_cmd, execute_cmd_out
 
 class BaseMacIpLearningTest(GenericTestBase):
@@ -152,3 +145,118 @@ class BaseMacIpLearningTest(GenericTestBase):
         else:
             return None
     # end check_bfd_state
+
+    def config_bfd_on_crpd(
+            self,
+            vm,
+            gw_ip,
+            containerIP,
+            lo_ip='100.100.100.100',
+            container_name='crpd01'):
+        '''
+        Set config on the cRPD Container
+        '''
+        cmd = ['docker exec %s cli -c\
+            "edit;\
+                set interfaces eth0 unit 0 family inet address %s;\
+                    set interfaces lo0 unit 0 family inet address %s;\
+                        set routing-options static route 0.0.0.0/0 next-hop %s;\
+                            set routing-options static route 0.0.0.0/0 bfd-liveness-detection minimum-interval 300;\
+                                commit"' \
+                                    % (container_name, containerIP, lo_ip, gw_ip)]
+
+        out = vm.run_cmd_on_vm(cmd, as_sudo=True)
+    # end config_bfd_on_crpd    
+    
+    def get_docker_image(
+            self,
+            vm,
+            image_name='crpd'):
+        '''
+        Get cRPD Container Image
+        '''
+        cmd = ['docker images | grep %s' % image_name]
+        out = vm.run_cmd_on_vm(cmd, as_sudo=True)
+        for val in out.values():
+            image = val.split()[1]
+        return image
+
+    # End of get_crpd_image
+
+    def create_crpd_container(
+            self,
+            vm,
+            container_ip,
+            network_name='ipvlannet',
+            container_name='crpd01'):
+        '''
+        Create cRPD Container
+        '''
+        cmds = ['sudo docker volume create %s-config &&'
+                'sudo docker volume create %s-varlog' % (container_name, container_name)]
+        vm.run_cmd_on_vm(cmds, as_sudo=True)
+        
+        cmd2 = ['docker run --rm --detach --name %s -h %s --net=%s --ip=%s --privileged -v %s-config:/config -v %s-varlog:/var/log -it crpd:%s' % (container_name, container_name, network_name, container_ip, container_name, container_name, self.get_docker_image(vm))]
+
+        out = vm.run_cmd_on_vm(cmd2, as_sudo=True)
+
+    # End create_crpd_container
+    
+    def create_crpd_network(
+            self,
+            vm,
+            subnet,
+            gateway,
+            name='ipvlannet',
+            ipvlan_mode=None):
+        '''
+        Create cRPD Network
+        '''
+        if ipvlan_mode:
+            cmd1 = ['docker network create -d ipvlan -o ipvlan_mode=%s --subnet=%s --gateway=%s -o parent=eth0 %s' % (ipvlan_mode, subnet, gateway, name)]
+        else:
+            cmd1 = ['docker network create -d ipvlan --subnet=%s --gateway=%s -o parent=eth0 %s' % (subnet, gateway, name)]
+
+        out = vm.run_cmd_on_vm(cmd1, as_sudo=True)
+
+    # End create_crpd_network
+      
+    def get_bfd_state_crpd(
+            self,
+            vm,
+            container_name='crpd01'):
+        '''
+        Returns intf mac addr on vSRX
+        '''
+        cmd = ['docker exec %s cli -c "show bfd session"' % container_name]
+        output = vm.run_cmd_on_vm(cmd, as_sudo=True)
+        for var in output.values():
+            output = var.split()
+        if "Up" in output:
+            return "Up"
+        elif "Down" in output:
+            return "Down"
+        else:
+            return None
+    # end check_bfd_state
+
+    def ping_from_crpd_container(
+            self,
+            vm,
+            dest_ip,
+            count='3',
+            container_name='crpd01'):
+        '''
+        Ping from the cRPD Container
+        '''
+        cmd = ['docker exec -it %s ping %s -c %s' % (container_name, dest_ip, count)]
+        output = vm.run_cmd_on_vm(cmd, as_sudo=True)
+        for var in output.values():
+            output = var.split(',')
+        if " 100% packet loss" in output:
+            return False
+        else:
+            return True
+
+    # end ping_from_crpd_container
+    
